a6494244891b0712b0c6c283e71dd46fd24ccf99
Amos Benari <abenari@redhat.com>
2013-08-21 15:42:40 +0300

    simple wizard for create content view.

--- app/helpers/content/content_views_helper.rb
+++ app/helpers/content/content_views_helper.rb
@@ -9,5 +9,13 @@ module Content
       end
     end
 
+    def next_or_cancel path, args={}
+      args[:cancel_path] ||= send("#{controller_name}_path")
+      content_tag(:div, :class => "form-actions") do
+        link_to(_("Cancel"), args[:cancel_path], :class => "btn") + " " +
+        submit_tag( _("Next"), :class => "btn btn-primary")
+      end
+    end
+
   end
 end
