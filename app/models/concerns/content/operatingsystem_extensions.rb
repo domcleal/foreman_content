53b367db7cd10ee376e913cf83a74a8bb7ca6cf5
Ohad Levy <ohadlevy@redhat.com>
2013-08-28 14:00:02 +0300

    typo

--- app/models/concerns/content/operatingsystem_extensions.rb
+++ app/models/concerns/content/operatingsystem_extensions.rb
@@ -4,7 +4,7 @@ module Content::OperatingsystemExtensions
   included do
     has_many :repositories, :class_name => 'Content::Repository'
 
-    has_many :available_content_veiws, :dependent => :destroy, :class_name => 'Content::AvailableContentView'
+    has_many :available_content_views, :dependent => :destroy, :class_name => 'Content::AvailableContentView'
     has_many :content_views, :as => :originator, :class_name => 'Content::ContentView'
 
     scope :has_repos, joins(:repositories).uniq
