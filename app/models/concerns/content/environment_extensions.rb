b66832cfb2bf192400db661594dff780c0ad03b8
Ohad Levy <ohadlevy@redhat.com>
2013-08-28 09:58:18 +0300

    typo

--- app/models/concerns/content/environment_extensions.rb
+++ app/models/concerns/content/environment_extensions.rb
@@ -2,8 +2,8 @@ module Content::EnvironmentExtensions
   extend ActiveSupport::Concern
 
   included do
-    has_many :available_content_veiws, :dependent => :destroy, :class_name => 'Content::AvailableContentView'
-    has_many :content_views, :through => :available_content_veiws, :class_name => 'Content::ContentView'
+    has_many :available_content_views, :dependent => :destroy, :class_name => 'Content::AvailableContentView'
+    has_many :content_views, :through => :available_content_views, :class_name => 'Content::ContentView'
   end
 
 end
